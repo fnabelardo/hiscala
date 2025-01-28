9f244d4deb1c88567896e051cf019ecc88b747cd
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-01-28 10:58:41 -0500

    feat: Print string with non type interpolation

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -4,6 +4,6 @@ object Demo {
   def main(args: Array[String]): Unit = {
     val name = "Noel"
     val age = 37
-    println(name + " is " + age + " years old")
+    println(s"$name is $age years old")
   }
 }

f7af501bc533cad9768ad5b8a8e0d9d50bbb06c4
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-01-28 11:29:21 -0500

    feat: Use || statements inside If else statements

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -6,7 +6,7 @@ object Demo {
     val y = 30;
     var res = "";
 
-    if (x == 20 && y == 30) {
+    if (x == 20 || y == 30) {
       res = "x == 20 && y == 30"
     } else {
       res = "x != 20"

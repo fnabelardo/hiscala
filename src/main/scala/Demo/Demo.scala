259adf1823f6c86427a405d0a18924ce3538ec5d
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-01-28 17:24:20 -0500

    feat: Use match as statements

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -2,39 +2,17 @@ package Demo
 
 object Demo {
   def main(args: Array[String]): Unit = {
+    val age = 50
 
-    // Loop with range "to"
-    for (i <- 1 to 5) {
-      println("i using to " + i)
+    // Match as statements
+    age match {
+      case 20 => println("age: " + age);
+      case 18 => println("age: " + age);
+      case 30 => println("age: " + age);
+      case 40 => println("age: " + age);
+      case 50 => println("age: " + age);
+      case _ => println("default");
     }
-
-    // Loop with range "until"
-    for (i <- 1 until 6) {
-      println("i using until " + i)
-    }
-
-    // Nested for loop
-    for (i <- 1 until 9; j <- 1 to 3) {
-      println("i using until " + i + " " + j)
-    }
-
-    // List as range in for loops
-    val lst = List(1,2,3,4,5,6,4,5,6,98)
-    for (i <- lst) {
-      println("i using lst " + i)
-    }
-
-    // List as range and if as filter in for loops
-    for (i <- lst; if i < 6) {
-      println("i using lst " + i)
-    }
-
-    // Loop as expression - Result: List
-    val result = for {i <- lst; if i < 6} yield {
-      i * i
-    }
-
-    println("result " + result)
-
+    
   }
 }

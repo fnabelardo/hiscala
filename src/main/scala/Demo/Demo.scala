5c61378a6bb291af579559fcbfe1ec29d2426829
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-01-28 11:03:26 -0500

    feat: Print string with new line

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -3,7 +3,9 @@ package Demo
 object Demo {
   def main(args: Array[String]): Unit = {
     val name = "Noel"
-    val age = 37
+    val age = 37.5
     println(s"$name is $age years old")
+    println(f"$name%s is $age%f years old")
+    println(s"Hello \nworld")
   }
 }

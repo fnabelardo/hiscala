d7482920c6227652e64c1a0a9738d09a111ee796
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-03 14:21:36 -0500

    feat: Create anonymous function

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -10,19 +10,10 @@ object Demo {
     def square(x : Int) = x * x
   }
 
-  def subtract(x : Int, y : Int) : Int = {
-    x - y;
-  }
-
-  def multiply(x : Int, y : Int) : Int = x * y;
-
-  def divide(x : Int, y : Int) = x / y;
-
   def main(args: Array[String]): Unit = {
+    var add = (x: Int, y: Int) => x + y;
+    println(add(300, 200))
     println("Math add: " + Math.add(45, 15))
-    println("Math square: " + Math.square(3));
-    println("subtract: " + subtract(45, 15))
-    println("multiply: " + multiply(45, 15))
-    println("divide: " + divide(45, 15))
+    println("Math square: " + (Math square 3));
   }
 }

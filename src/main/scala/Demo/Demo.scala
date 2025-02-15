d4e9beb5703c5b5cb6e0a60f996c97fbed09d359
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-15 16:48:54 -0500

    feat: Create fully applied function

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -1,18 +1,10 @@
 package Demo
 
+// Scala - Fully Applied Functions
 object Demo {
 
-  def math(x: Double, y: Double, z: Double, f: (Double, Double) => Double): Double = f(f(x, y), z);
-
   def main(args: Array[String]): Unit = {
-    println(math(50, 20, 10, (x, y) => x + y))
-    println(math(50, 20, 10, _+_)) // Equals to println(math(50, 20, 10, (x, y) => x + y))
-    println(math(50, 20, 10, (x, y) => x * y))
-    println(math(50, 20, 10, _*_)) // Equals to println(math(50, 20, 10, (x, y) => x * y))
-    println(math(50, 20, 10, (x, y) => x max y))
-    println(math(50, 20, 10, _ max _))// Equals to println(math(50, 20, 10, (x, y) => x max y))
-    println(math(50, 20, 10, (x, y) => x min y))
-    println(math(50, 20, 10, _ min _))
-
+    val sum = (a: Int, b: Int, c: Int) => a + b + c
+    println(sum(10, 20, 30))
   }
 }

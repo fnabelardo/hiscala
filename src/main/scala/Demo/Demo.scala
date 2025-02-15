8fe8603c3d952ef32cab81386c59c012dbe3d1fd
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-15 18:18:01 -0500

    feat: Use closures

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -1,28 +1,14 @@
 package Demo
 
-// Scala - Partially Applied Functions
+// Scala - Closures
+/* A closure is a function which uses one or more
+ variables declared outside this function. */
 import java.util.Date
 object Demo {
-
-  //Real world example
-  def log(date : Date, message : String): Unit = {
-    println(date.toString + "  " + message)
-  }
+  var number = 10;
+  val add = (x : Int) => x + number;
 
   def main(args: Array[String]): Unit = {
-    val sum = (a: Int, b: Int, c: Int) => a + b + c
-    // _: Wild card
-    val f = sum(10, 20, _ : Int)
-    val y = sum(10, _: Int, _ : Int)
-    println(f(30)) //Output: 60
-    println(y(40, 30)) //Output: 80
-
-    //Real world example
-    val date = new Date;
-    val newLog = log(date, _ :String)
-    newLog("The message 1");
-    newLog("The message 2");
-    newLog("The message 3");
-    newLog("The message 4");
+    println(add(20)) //Output: 30
   }
 }

c867cd9c7ed1db4d0f1f3762091af1448f991b48
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-15 18:47:13 -0500

    feat: Create Curried function

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -1,14 +1,16 @@
 package Demo
 
-// Scala - Closures
-/* A closure is a function which uses one or more
- variables declared outside this function. */
-import java.util.Date
+// Scala - Currying
+/* Currying is the technique of transforming a function
+   that takes multiple arguments into a function
+   that takes a single argument. */
 object Demo {
-  var number = 10;
-  val add = (x : Int) => x + number;
+  def add = (x: Int, y: Int) => x + y;
+
+  def add2(x: Int) = (y: Int) => x + y;
 
   def main(args: Array[String]): Unit = {
-    println(add(20)) //Output: 30
+    println(add(20, 10)) //Output: 30
+    println(add2(20)(30)); //Output: 50
   }
 }

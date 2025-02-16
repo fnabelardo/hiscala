4aa1cdf556220b165efa86cc6e8093c536510f11
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-15 19:20:40 -0500

    feat: Lets another parameter to going to pass later

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -6,8 +6,8 @@ package Demo
    that takes a single argument. */
 object Demo {
   def add = (x: Int, y: Int) => x + y;
-
   def add2(x: Int) = (y: Int) => x + y;
+  def add3 (x: Int) (y: Int) = x + y; //Another definition for Curried function
 
   def main(args: Array[String]): Unit = {
     println(add(20, 10)) //Output: 30
@@ -15,5 +15,8 @@ object Demo {
 
     val sum40 = add2(40);
     println(sum40(100)); //Output: 140
+
+    val sum50 = add3(50)_;//_ Means a second argument that we are going to pass later
+    println(sum50(400));//Output: 450
   }
 }

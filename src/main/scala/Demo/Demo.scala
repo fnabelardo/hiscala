5a8444f15195ac9b2e4ed40749f3ce189583815c
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-17 13:58:32 -0500

    feat: Concat and print array values

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -1,9 +1,13 @@
 package Demo
 
+import Array._
+
 // Scala - Arrays
 object Demo {
-  var myarray: Array[Int] = new Array[Int](4)//Declaring Array
-  var myarray2 = new Array[Int](5)//Declaring Array
+  var myarray: Array[Int] = new Array[Int](4) //Declaring Array
+  var myarray2 = new Array[Int](5) //Declaring Array
+  var myarray3 = Array(1, 2, 3, 5, 8, 9); //Declaring Array
+
   def main(args: Array[String]): Unit = {
     myarray(0) = 20;
     myarray(1) = 50;
@@ -15,8 +19,15 @@ object Demo {
       println(x)
     }
     //Print: The array
-    for (i <- 0 to (myarray.length -1)) {
+    for (i <- 0 to (myarray.length - 1)) {
       println(myarray(i))
+    }
+
+    //Concat arrays
+    val result = concat(myarray, myarray3);
+    println("Concat arrays")
+    for (x <- result) {
+      println(x)
     }
   }
 }

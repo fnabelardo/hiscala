4e4071a90a14f3aa31aec4239563e826a0e1c24e
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-17 11:57:12 -0500

    feat: Format strings

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -3,6 +3,8 @@ package Demo
 // Scala - String
 object Demo {
 
+  val num1 = 75;
+  val num2 = 100.25;
   val str1 : String = "Hello World"
   val str2 : String = " Noel"
 
@@ -10,5 +12,10 @@ object Demo {
     println(str1.length())
     println(str1.concat(str2))
     println(str1 + str2)
+
+    val result = printf("(%d --%f --%s)", num1, num2, str1);
+    println(result);//Output: (75 --100.250000 --Hello World)()
+    println("(%d --%f --%s)".format(num1, num2, str1));//Output: (75 --100.250000 --Hello World)
+    printf("(%d --%f --%s)", num1, num2, str1);//Output: (75 --100.250000 --Hello World)
   }
 }

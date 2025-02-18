2b3c6f92dd8b2d5088fd3fc508b5d4d4a6c9cf30
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-18 18:29:20 -0500

    feat: Declare Map and get value by key

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -1,46 +1,15 @@
 package Demo
 
-import scala.collection.mutable
-
-// Scala - Sets
-/* The Set is the collection elements of same data types.
-* That contain no duplicate elements */
+// Scala - Maps
+/* A Map is an Iterable consisting of pairs of keys and values */
 object Demo {
-  //Immutable set
-  val immutableSet : Set[Int] = Set(1, 2, 5, 8, 9, 2)
-  val immutableSet2 : Set[Int] = Set(4, 42, 6, 8, 19, 25, 7)
-  //Mutable set
-  var mutableSet: mutable.Set[Int] = mutable.Set(1, 2, 4, 5, 7, 7)
-  val names: Set[String] = Set("Jhon", "Doe", "Jane")
+
+  val myMap : Map[Int, String] =
+    Map(801 -> "Max", 802 -> "Jhon", 804 -> "Doe")
 
   def main(args: Array[String]): Unit = {
-    //Add the value to set without persist
-    println(immutableSet + 10); //Output: HashSet(5, 10, 1, 9, 2, 8)
-    println(immutableSet);      //Output: HashSet(5, 1, 9, 2, 8)
-
-    //Check if value exists
-    println(immutableSet(9))      //Output: true.
-    println(immutableSet(99))      //Output: false.
-
-    //Methods over sets: head, tail, isEmpty
-    println(immutableSet.head)
-    println(immutableSet.tail)
-    println(immutableSet.isEmpty)
-
-    //Concatenate Sets
-    println(immutableSet ++ immutableSet2)      //Output: HashSet(5, 1, 6, 9, 2, 7, 8, 19, 4, 42, 25)
-    //Or
-    println(immutableSet++(immutableSet2))
-
-    //Intersection Sets
-    println(immutableSet.&(immutableSet2))          //Output: HashSet(8)
-    //Or
-    println(immutableSet.intersect(immutableSet2))  //Output: HashSet(8)
-
-    //Loop over set
-    immutableSet.foreach(println)
-    for (name <- names) {
-      println(name)
-    }
+    println(myMap) //Output: Map(801 -> Max, 802 -> Jhon, 804 -> Walter)
+    //Get value by key
+    println(myMap(802)); //Output: Jhon
   }
 }

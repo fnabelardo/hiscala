fb872f6997a395f6039bfd44f482849ef0fa128e
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-18 17:42:45 -0500

    feat: Add the value to set without persist

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -9,12 +9,12 @@ object Demo {
   //Immutable set
   val immutableSet : Set[Int] = Set(1, 2, 5, 8, 9, 2)
   //Mutable set
-  //var mutableSet = scala.collection.mutable.Set(1, 2, 4, 5, 7, 7)
   var mutableSet: mutable.Set[Int] = mutable.Set(1, 2, 4, 5, 7, 7)
   val names: Set[String] = Set("Jhon", "Doe", "Jane")
 
   def main(args: Array[String]): Unit = {
-    println(immutableSet);
-    println(mutableSet);
+    //Add the value to set without persist
+    println(immutableSet + 10); //Output: HashSet(5, 10, 1, 9, 2, 8)
+    println(immutableSet);      //Output: HashSet(5, 1, 9, 2, 8)
   }
 }

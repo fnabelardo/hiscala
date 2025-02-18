9b1fdf4530d309a809420a4bc2558fe0a856ec27
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-18 17:35:40 -0500

    feat: Declare mutable and immutable sets

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -1,52 +1,20 @@
 package Demo
 
-// Scala - Lists
-/* The List are immutable, we don't can change de values */
+import scala.collection.mutable
+
+// Scala - Sets
+/* The Set is the collection elements of same data types.
+* That contain no duplicate elements */
 object Demo {
-  val mylist: List[Int] = List(1, 2, 4, 5, 7, 9)
-  val names: List[String] = List("Jhon", "Doe", "Jane")
+  //Immutable set
+  val immutableSet : Set[Int] = Set(1, 2, 5, 8, 9, 2)
+  //Mutable set
+  //var mutableSet = scala.collection.mutable.Set(1, 2, 4, 5, 7, 7)
+  var mutableSet: mutable.Set[Int] = mutable.Set(1, 2, 4, 5, 7, 7)
+  val names: Set[String] = Set("Jhon", "Doe", "Jane")
 
   def main(args: Array[String]): Unit = {
-    println("Numbers list");
-    println(mylist);
-    println("Names list");
-    println(names);
-
-    //Prepend 0 to the first place in the list
-    println(0 :: mylist)
-    //Create list with prepended values to Nil
-    println(1 :: 3 :: 8 :: Nil)
-
-    //Get the first value of the list
-    println(mylist.head)
-
-    //Get the last value of the list
-    println(names.last)
-
-    //Check if the list is Empty
-    println(mylist.isEmpty)
-
-    //Get the reverse list
-    println(names.reverse)
-
-    //Fill the list
-    println(List.fill(5)(3))
-
-    //Max of the list
-    println(mylist.max);
-
-    mylist.foreach(println)
-    //Sum the elements list
-    var sum : Int = 0
-    mylist.foreach(sum += _)
-    print("Sum the elements list: ")
-    println(sum)
-
-    //For loop
-    println("Element list")
-    for(name <- names) {
-      println(name);
-    }
-
+    println(immutableSet);
+    println(mutableSet);
   }
 }

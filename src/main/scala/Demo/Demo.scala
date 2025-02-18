6b1a7cd2bd99e8b8801366c69f751ba03a87d38d
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-18 17:42:59 -0500

    feat: Check if value exists inside Set

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -16,5 +16,9 @@ object Demo {
     //Add the value to set without persist
     println(immutableSet + 10); //Output: HashSet(5, 10, 1, 9, 2, 8)
     println(immutableSet);      //Output: HashSet(5, 1, 9, 2, 8)
+
+    //Check if value exists
+    println(immutableSet(9))      //Output: true.
+    println(immutableSet(99))      //Output: false.
   }
 }

d012eca27dbeaade9a805ea80c177896f0d8dcef
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-18 19:18:14 -0500

    feat: Concatenate Maps

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -4,8 +4,10 @@ package Demo
 /* A Map is an Iterable consisting of pairs of keys and values */
 object Demo {
 
-  val myMap : Map[Int, String] =
+  private val myMap: Map[Int, String] =
     Map(801 -> "Max", 802 -> "Jhon", 804 -> "Doe")
+  private val myMap2: Map[Int, String] =
+    Map(805 -> "Maxie", 806 -> "Jhonny")
 
   def main(args: Array[String]): Unit = {
     println(myMap) //Output: Map(801 -> Max, 802 -> Jhon, 804 -> Walter)
@@ -22,13 +24,16 @@ object Demo {
     println(myMap.isEmpty); //Output: false
 
     //Print Key and Value of Map
-    myMap.keys.foreach{ key =>
+    myMap.keys.foreach { key =>
       print("Key: " + key).toString;
       println(" Value: " + myMap(key));
     }
 
     //Check if Map contain a key
     println(myMap.contains(801)); //Output: true
+
+    //Concatenate Maps
+    println(myMap ++ myMap2); //Output: HashMap(802 -> Jhon, 806 -> Jhonny, 805 -> Maxie, 804 -> Doe, 801 -> Max)
   }
 
 }

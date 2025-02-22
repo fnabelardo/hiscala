f01999560027078024d89892f9672d39dfa7c540
Felix Noel Abelardo Santana <fnabelardo@gmail.com>
2025-02-22 14:15:31 -0500

    feat: Fetch element from the tuple inside another tuple

--- src/main/scala/Demo/Demo.scala
+++ src/main/scala/Demo/Demo.scala
@@ -7,21 +7,25 @@ object Demo {
   val myTuple = (1, 2, "Hello", true);
   val myTuple2 = new Tuple3(1, 2, "Hello");
   val myTuple3 = new Tuple2(1, "Hi");
+  val myTuple4 = new Tuple3(1, "Hi", (3, 4));
 
   def main(args: Array[String]): Unit = {
     println(myTuple._1)
     println(myTuple3._2)
 
     //Iterate over the tuple
-    myTuple.productIterator.foreach{
+    myTuple.productIterator.foreach {
       i => println(i)
     }
 
     //Other ways to declare tuples
     //Create tuple with 2 elements
-    println(1 -> "Noel")//Output: (1,Noel)
+    println(1 -> "Noel") //Output: (1,Noel)
     //Create tuple with 3 elements, 1 tuple and another element
-    println(1 -> "Noel" -> false)//Output: ((1,Noel),false)
+    println(1 -> "Noel" -> false) //Output: ((1,Noel),false)
+
+    //Fetch element from the tuple inside another tuple: ((1,Hi,(3,4)))
+    print(myTuple4._3._2) //Output: 4
   }
 
 }
